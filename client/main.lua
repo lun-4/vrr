d658bc19d5a8ab2a2ff77056fb73c2be17a0dd17
Luna <git@l4.pm>
2022-01-24 00:19:13 -0300

    add controllers

--- client/main.lua
+++ client/main.lua
@@ -2,7 +2,17 @@
 local lovr = require('lovr')
 local loglib = require('log')
 
-ctx = {}
+ctx = {
+    last_controller_position = {
+        left = nil,
+        right = nil,
+    },
+
+    active_controller = {
+        left = true,
+        right = true,
+    }
+}
 
 function lovr.load()
     local log_thread = loglib.startLogThread()
@@ -11,13 +21,21 @@ function lovr.load()
         lovr.event.quit(1)
     end
 
+    ctx.models = {}
+    if lovr.headset.getDriver() ~= "desktop" then
+        src_path = lovr.filesystem.getSource()
+        print('src path:', src_path)
+        ctx.models.left = lovr.graphics.newModel('quest2_left_hand.glb')
+        ctx.models.right = lovr.graphics.newModel('quest2_right_hand.glb')
+    end
+
     ctx.image_1 = lovr.data.newImage(1366, 768, 'rgb', nil)
-    ctx.texture_1 = lovr.graphics.newTexture(ctx.image_1, {type='2d', msaa=0, mipmaps=false})
+    ctx.texture_1 = lovr.graphics.newTexture(ctx.image_1, {type='2d', msaa=8, mipmaps=false})
     ctx.material_1 = lovr.graphics.newMaterial(ctx.texture_1)
 
-    ctx.image_2 = lovr.data.newImage(1080, 1920, 'rgb', nil)
-    ctx.texture_2 = lovr.graphics.newTexture(ctx.image_2, {type='2d', msaa=0, mipmaps=false})
-    ctx.material_2 = lovr.graphics.newMaterial(ctx.texture_2)
+    --ctx.image_2 = lovr.data.newImage(1080, 1920, 'rgb', nil)
+    --ctx.texture_2 = lovr.graphics.newTexture(ctx.image_2, {type='2d', msaa=8, mipmaps=false})
+    --ctx.material_2 = lovr.graphics.newMaterial(ctx.texture_2)
 
     ctx.screen_1_in = lovr.thread.getChannel('screen_1_in')
     ctx.screen_2_in = lovr.thread.getChannel('screen_2_in')
@@ -67,6 +85,54 @@ function lovr.load()
 end
 
 function lovr.update()
+
+    for hand, model in pairs(ctx.models) do
+        if lovr.headset.isTracked(hand) then
+            local pose = {lovr.headset.getPose(hand)}
+            local current_position = vec3(pose[1], pose[2], pose[3])
+            local last_position = ctx.last_controller_position[hand]
+            print('cur', current_position:unpack())
+            print('last', last_position)
+            if last_position == nil then
+                print('new position!', current_position:unpack())
+                ctx.last_controller_position[hand] = {
+                    lovr.timer.getTime(),
+                    {current_position:unpack()},
+                }
+            elseif last_position ~= nil then
+                local delta_vec = current_position:sub(vec3(unpack(last_position[2])))
+                local dx, dy, dz = delta_vec:unpack()
+                local average_delta = (dx+dy+dz / 3)
+                print('delta_vec', delta_vec:unpack())
+                print('avg', average_delta)
+
+                -- movement detected
+                if average_delta > 0.1 then
+                    print('new position!', current_position:unpack())
+                    ctx.last_controller_position[hand] = {
+                        lovr.timer.getTime(),
+                        {current_position:unpack()},
+                    }
+                    if not ctx.active_controller[hand] then
+                        ctx.active_controller[hand] = true
+                    end
+                else
+                    local last_timestamp = last_position[1]
+                    local current_timestamp = lovr.timer.getTime()
+                    print('checking last ts!', last_timestamp, current_timestamp)
+
+                    local delta = current_timestamp - last_timestamp
+                    print('inactive delta', delta)
+
+                    if delta > 3 and ctx.active_controller[hand] then
+                        print(hand, 'inactive')
+                        ctx.active_controller[hand] = false
+                    end
+                end
+            end
+        end
+    end
+
     --local blob = ctx.image:getBlob()
     --local blob_ptr = blob:getPointer()
 
@@ -91,9 +157,15 @@ function lovr.draw()
     --     ctx.image:setPixel(30, 30 + i, 1, 1, 1)
     -- end
     ctx.texture_1:replacePixels(ctx.image_1)
-    ctx.texture_2:replacePixels(ctx.image_2)
-    lovr.graphics.plane(ctx.material_1, 0, 1.4, -2, 1.77, 1, math.pi, 1, 0, 0)
-    lovr.graphics.plane(ctx.material_2, 1.56, 1.4, -2, 1.125, 2, math.pi, 1, 0, 0)
+    --ctx.texture_2:replacePixels(ctx.image_2)
+    lovr.graphics.plane(ctx.material_1, 0, 1.5, -3, 3.55, 2, math.pi, 1, 0, 0)
+    --lovr.graphics.plane(ctx.material_2, 1.56, 1.4, -2, 1.125, 2, math.pi, 1, 0, 0)
+
+    for hand, model in pairs(ctx.models) do
+        if ctx.active_controller[hand] and lovr.headset.isTracked(hand) then
+            model:draw(mat4(lovr.headset.getPose(hand)))
+        end
+    end
 end
 
 
